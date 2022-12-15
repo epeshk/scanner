9d25e0ccbd9ce4d955e4f54c5269acae71220fbd
epeshk <>
2022-12-15 21:40:28 +0300

    Added new much shorter Scanners

---
+++ Epeshk.Text.Reduced/AsciiScanner2.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Epeshk.Text
+{
+  using System;
+  using System.Buffers.Text;
+  using System.IO;
+
+  public class AsciiScanner2
+  {
+    private const int MaxTokenLength = 1200;
+
+    Stream? input = Console.OpenStandardInput();
+    byte[] buffer = new byte[32768];
+    Span<byte> Fragment => buffer.AsSpan(offset, length - offset);
+
+    int offset;
+    int length;
+
+    public int ReadInt()
+    {
+      while (input != null && length - offset < MaxTokenLength)
+      {
+        if (offset != 0)
+        {
+          var remaining = Fragment.Length;
+          Fragment.CopyTo(buffer);
+          offset = 0;
+          length = remaining;
+        }
+
+        var count = input.Read(buffer, length, buffer.Length - length);
+        if (count <= 0)
+        {
+          input = null;
+          break;
+        }
+
+        length += count;
+        while (offset < length && buffer[offset] <= ' ') offset++;
+      }
+      while (offset < length && buffer[offset] <= ' ') offset++;
+
+      var parsed = Utf8Parser.TryParse(Fragment, out int value, out int bytesConsumed);
+      if (!parsed)
+        Throw();
+      offset += bytesConsumed;
+      return value;
+    }
+
+    void Throw() => throw new Exception();
+  }
+}
---
+++ Epeshk.Text.Reduced/TextScanner2.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Epeshk.Text
+{
+  using System;
+  using System.Globalization;
+  using System.IO;
+
+  public class TextScanner2
+  {
+    StreamReader input = new StreamReader(Console.OpenStandardInput(), bufferSize: 16384);
+    char[] buffer = new char[4096];
+
+    public int ReadInt()
+    {
+      int length = 0;
+      bool readStart = false;
+      while (true)
+      {
+        int ch = input.Read();
+        if (ch == -1)
+          break;
+
+        if (char.IsWhiteSpace((char)ch))
+        {
+          if (readStart) break;
+          continue;
+        }
+
+        readStart = true;
+        buffer[length++] = (char)ch;
+      }
+
+      return int.Parse(buffer.AsSpan(0, length));
+    }
+  }
+}
